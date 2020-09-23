292568ca6f075c3581bb7a869c8a644855c09416
nslythe <nicolas@slythe.net>
2020-09-23 12:16:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
-sudo apt-get update
+apt-get update
 
 # iptable
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt-get install -y iptables-persistent
 firewall.sh
+
+# install dev
+apt-get install -y ruby
+gem install bundler jekyll
 
 # docker
 apt-get install -y docker.io
